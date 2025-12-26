c3e8e6cab70490e4f9b917268007e6f0e0cd8f92
Dara <206672864+Daraga123@users.noreply.github.com>
2025-12-27 00:32:14 +0700

    Add greeting functionality to button click
    Fix typo in style properties and add greeting functionality.

---
+++ project-001/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('btn-greet');
+const display = document.getElementById("message");
+
+button.addEventListener('click', function() {
+                        display.textContent="Selamat, Anda baru saja mengatifkan javascript pertama anda.";
+                        display.style.color='007dff';
+                        display,style.fontWeidth='bold';
+                        });
