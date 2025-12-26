31776e9f3ebe673f03fb1278a69bb3a8fa9ed391
Dara <206672864+Daraga123@users.noreply.github.com>
2025-12-27 01:31:22 +0700

    Merapikan tata letak

--- project/project-001/script.js
+++ project/project-001/script.js
@@ -1,8 +1,9 @@
-const button = document.getElementById('btn-greet');
+const button = document.getElementById("btn-greet");
 const display = document.getElementById("message");
 
-button.addEventListener('click', function() {
-                        display.textContent="Selamat, Anda baru saja mengatifkan javascript pertama anda.";
-                        display.style.color='007dff';
-                        display,style.fontWeidth='bold';
-                        });
+button.addEventListener("click", function () {
+  display.textContent =
+    "Selamat, Anda baru saja mengatifkan javascript pertama anda.";
+  display.style.color = "007dff";
+  display, (style.fontWeidth = "bold");
+});
